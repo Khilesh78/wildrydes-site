3fb34042d163480cb01833adac53b8213c3e7d8e
KhILeSh_01 <khilesh7898@gmail.com>
2026-06-06 17:52:18 +0530

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_U8HHNFR3u', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '67tvmpmt7lcsatk7anq2bbiesn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_n7ISO0rb8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '271gfb3jqsgtqmm5g0fuf1sfs1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
